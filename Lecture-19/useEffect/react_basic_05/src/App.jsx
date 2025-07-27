0b5000309e86cf779008fcb84feb29a4da34a35d
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-07-27 17:56:28 +0530

    lecture 19 react codes

---
+++ Lecture-19/useEffect/react_basic_05/src/App.jsx
@@ -0,0 +1,14 @@
+import ApiCalling from "./components/ApiCalling";
+import Effects from "./components/Effects";
+
+function App(){
+  return(
+    <div>
+      <h2>Hello from App</h2>
+      {/* <Effects /> */}
+      <ApiCalling />
+    </div>
+  )
+}
+
+export default App;
