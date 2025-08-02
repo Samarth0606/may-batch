ef2d7be82415ce5b3f511355419dfb9891c383e5
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-08-02 17:38:13 +0530

    lecture 21 react codes

---
+++ Lecture-21/react_basic_07/src/App.jsx
@@ -0,0 +1,14 @@
+import Ref from "./components/Ref";
+import Reff from "./components/Reff";
+
+function App(){
+  return(
+    <div>
+      <h1>App</h1>
+      {/* <Ref /> */}
+      <Reff />
+    </div>
+  )
+}
+
+export default App;
