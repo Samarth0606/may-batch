d59da22d91dcd85620ad0af278742c1ea51bdd41
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-08-05 20:38:11 +0530

    lecture 2 react codes

--- Lecture-23/swiggy_fe_01/src/components/RestaurantCard.jsx
+++ Lecture-23/swiggy_fe_01/src/components/RestaurantCard.jsx
@@ -1,15 +1,20 @@
-function RestaurantCard(){
+function RestaurantCard({restArr}){
+
     return(
-        <div className="m-8">
-            <img className="w-58 h-42 rounded-2xl" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/a1e023e4-bdf3-4bcb-b4d9-2498ba52528e_1003414.JPG" alt="" />
-            <div className="m-2">
-                <h1 className="font-bold">PIZZA HUT</h1>
-                <span className="font-bold">4.3</span>
-                <span className="mx-4 font-bold">40-45 mins</span>
-                <p>Pizza's</p>
-                <p>Pasaria Road</p>
-            </div>
-        </div>
+        restArr.map((restDetails)=>{
+            return(
+                <div className="m-8 w-58">
+                    <img className="w-58 h-42 rounded-2xl" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restDetails.info.cloudinaryImageId}`} alt="" />
+                    <div className="m-2">
+                        <h1 className="font-bold">{restDetails.info.name} </h1>
+                        <span className="font-bold">{restDetails.info.avgRating}</span>
+                        <span className="mx-4 font-bold">{restDetails.info.sla.slaString}</span>
+                        <p>{restDetails.info.cuisines[0]}</p>
+                        <p>{restDetails.info.locality}</p>
+                    </div>
+                </div>
+            )
+        })
     )
 }
 
