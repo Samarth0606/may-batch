fe16c672641e7e0e58c286706b91f7f9c56d8797
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-08-10 18:34:21 +0530

    lecture 2 react project code

--- Lecture-24/swiggy_fe_02/src/App.jsx
+++ Lecture-24/swiggy_fe_02/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom"
 import Body from "./components/Body"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
@@ -6,7 +7,8 @@ function App(){
   return(
     <div>
       <Header />
-      <Body />
+      {/* <Body /> */}
+      <Outlet />
       <Footer />
 
 
