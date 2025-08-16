f03e1d70640a827e66030ad2cfe8f1c19037a585
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-08-16 19:34:44 +0530

    lecture 26 react codes

---
+++ Lecture-26/swiggy_fe_03/src/components/RestaurantDetail.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+
+function RestaurantDetail() {
+
+    const {idd} = useParams();
+    console.log(idd , "idd");
+    const [foodItemDetail,setFoodItemDetail] = useState([]); 
+    
+
+    useEffect(()=>{
+        const API = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&restaurantId=${idd}&submitAction=ENTER`
+        async function calling(){
+            let resp = await axios.get(API);
+            console.log(resp.data.data.cards[5].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards , "resp");
+            setFoodItemDetail(resp.data.data.cards[5].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards)
+        }
+        calling();
+    }, [idd])
+
+
+  return (
+    <div>
+        <h1 className='text-center font-bold text-2xl'>List of item's avaialable at restaurant</h1>
+        {
+            foodItemDetail.map((foodItem)=>{
+                return (
+                    <div className="flex w-3/4 mx-auto mb-10 border-b-4 p-4">
+                        <div className="flex flex-col w-3/4">
+                        <h1>{foodItem.card.info.name}</h1>
+                        <h1>{foodItem.card.info.price / 100}</h1>
+                        <h1>{foodItem.card.info.category}</h1>
+                        </div>
+                        <img
+                        className="w-52 h-44 rounded-md border shadow-lg border-gray-100"
+                        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${foodItem.card.info.imageId}`}
+                        alt=""
+                        />
+
+                        <button
+                        className="border bg-green-300 h-8 relative top-16 right-5"
+                        >
+                        Add +
+                        </button>
+                    </div>
+                );
+            })
+        }
+    </div>
+  )
+}
+
+export default RestaurantDetail
