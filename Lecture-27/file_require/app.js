628eccef9f234d87d3194ec2ac2557771eab00fd
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-08-17 18:36:47 +0530

    lecture 26 react codes

---
+++ Lecture-27/file_require/app.js
@@ -0,0 +1,16 @@
+export const fn = function(){
+    console.log("namaste");
+}
+
+export const obj = { a:10 }
+
+export const naam = "Samarth Vohra"
+
+console.log(naam , '1');
+console.log(obj , '1');
+fn()
+// module.exports = {} // default
+// module.exports = { fn: fn , obj: obj, naam: naam } 
+// module.exports = { fn , obj, naam } 
+
+
---
+++ Lecture-27/file_require/index.js
@@ -0,0 +1,25 @@
+
+// const xyz = require('./app')
+
+// console.log(xyz);
+// console.log(xyz.naam);
+// console.log(xyz.obj);
+// xyz.fn()
+
+// -------------------------------------
+
+// const {naam,obj,fn} = require('./app')
+
+// console.log(naam);
+// console.log(obj);
+// fn()
+
+
+// -------------------------------------
+
+// const {naam,obj,fn} = require('./app')
+import {naam,obj,fn} from './app.js'
+
+console.log(naam);
+console.log(obj);
+fn()
