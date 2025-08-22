a98e3ebccecad55d6d7bb7d419730a681ab57d1d
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-08-22 20:50:29 +0530

    lecture 29 react codes

--- Lecture-29/express/index.js
+++ Lecture-29/express/index.js
@@ -12,10 +12,33 @@
 // es module (newer way)
 import express from 'express'; // fn
 const app = express() // object -> main application
+// middleware = fn (which runs on every incoming request)
+// let amount = 700;
+// app.use( (req,res,next)=>{
+//     if(amount >= 800){
+//         next();
+//     }
+//     else{
+//         res.send("Sorry We cant help, complete request-response cycle")
+//     }
+// } )
+let amount = 700;
+app.use('/home', (req,res,next)=>{
+    if(amount >= 800){
+        next();
+    }
+    else{
+        res.send("Sorry We cant help, complete request-response cycle")
+    }
+} )
 
-app.get(' /', (req,res)=>{
+app.get('/', (req,res)=>{
     // res.send("Welcome to Root route")
     res.send("<h1>Welcome to Root route by nodemon</h1>")
+})
+// app.post('/home' , (req,res)=>{ 
+app.get('/home' , (req,res)=>{ 
+    res.send("Welcome HOME") 
 })
 
 const PORT = 8080;
